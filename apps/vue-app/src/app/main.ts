31c4ee60a37148ee208f36351b768eb7d64d2001
Reza Pourshariati <wwwshrreezzaash@gmail.com>
2026-08-15 15:32:39 +0330

    fix(client:auth): removed unused app layout component from main of vue-app

--- apps/vue-app/src/app/main.ts
+++ apps/vue-app/src/app/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import { setupPrimeVue } from '@/app/plugins/primevue'
 import router from '@/app/router'
-import AppLayout from '@/layouts/AppLayout.vue'
 import { createAppStore } from './store'
 import '@/assets/main.css'
 
@@ -12,5 +11,4 @@ const store = createAppStore()
 app.use(store)
 setupPrimeVue(app)
 app.use(router)
-app.component('AppLayout', AppLayout)
 app.mount('#app')
